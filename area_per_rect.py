202ce9cb4b045e4a18227e6e54bb0f380baf2339
Krostofer <christopher.di-bert@stu.ocsb.ca>
2022-09-17 15:11:32 +0000

    This program calculates the area and perimeter of a rectangle with dimensions 9cm x 5cm.

---
+++ area_per_rect.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created By: Chris Di Bert
+# Date: Sep. 17, 2022
+# This program calculates the area and perimeter of a rectangle with dimensions
+# 9cm x 5cm
+
+
+def main():
+    print("If the rectangle has the dimensions:")
+    print("9cm x 5cm")
+    print("")
+    print("The area would be {}cm^2".format(9 * 5))
+    print("The perimeter would be {}cm".format(2 * (9 + 5)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created By: Chris Di Bert
+# Date: Sep. 17, 2022
+# This program performs basic math operations.
+import math
+
+
+def main():
+    print("9+11.2 = {}".format(9 + 11.2))
+    print("36.3-8.4 = {}".format(36.3 - 8.4))
+    print("12.6*3 = {}".format(12.6 * 3))
+    print("7/5 = {}".format(7 / 5))
+    print("3.3**3 = {}".format(3.3**3))
+    print("√5 = {}".format(math.sqrt(5)))
+
+
+if __name__ == "__main__":
+    main()
